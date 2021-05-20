f4b61aa02595bf20362a215b67f0355f1be00903
luan francisco lima fernandes <luanfranciscojr@gmail.com>
2021-05-20 00:50:52 -0300

    Removendo comentarios

--- app/src/main/java/com/example/twitteranalyzer/di/ViewModelModule.kt
+++ app/src/main/java/com/example/twitteranalyzer/di/ViewModelModule.kt
@@ -7,7 +7,6 @@ import org.koin.dsl.module
 
 val viewModelModule = module {
 
-    // Specific viewModel pattern to tell Koin how to build CountriesViewModel
     viewModel {
         TwitterListViewModel(repository = get())
     }
